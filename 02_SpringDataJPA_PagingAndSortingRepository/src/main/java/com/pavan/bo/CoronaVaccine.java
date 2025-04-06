3077d6b396afe2e7e9aa36539a54605e50c9e1ad
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 04:47:15 +0530

    SpringDataJpa Project To Work With PaggingAndSortingRepository

---
+++ 02_SpringDataJPA_PagingAndSortingRepository/src/main/java/com/pavan/bo/CoronaVaccine.java
@@ -0,0 +1,12 @@
+package com.pavan.bo;
+
+import java.io.Serializable;
+
+public class CoronaVaccine implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
