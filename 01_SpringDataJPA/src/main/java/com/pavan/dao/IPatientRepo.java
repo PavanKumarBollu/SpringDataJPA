da4f62f479186c48e66ebcc3039866040fbd9e1a
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 02:40:48 +0530

    Sprind Data JPa Project to work with PatientTable of HMS

--- 01_SpringDataJPA/src/main/java/com/pavan/dao/IPatientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pavan.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.pavan.bo.Patient;
-
-public interface IPatientRepo extends CrudRepository<Patient, Long> {
-
-}
