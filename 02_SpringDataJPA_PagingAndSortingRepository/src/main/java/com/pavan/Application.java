24666c28399686ec86a4a164db1619b82ed40a25
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 05:13:11 +0530

    SpringDataJpa Project To Work With PaggingAndSortingRepository

--- 02_SpringDataJPA_PagingAndSortingRepository/src/main/java/com/pavan/Application.java
+++ 02_SpringDataJPA_PagingAndSortingRepository/src/main/java/com/pavan/Application.java
@@ -2,12 +2,31 @@ package com.pavan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.pavan.bo.CoronaVaccine;
+import com.pavan.service.CoronoVaccineMgmtServiceImpl;
+import com.pavan.service.ICoronaVaccineMgmtService;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		
+		
+		ICoronaVaccineMgmtService service = context.getBean(CoronoVaccineMgmtServiceImpl.class);
+		Iterable<CoronaVaccine> fetchDetails = service.fetchDetails(true, "price", "name");
+		fetchDetails.forEach(vaccine->{
+			System.out.println( vaccine.getName() + " == " + vaccine.getPrice());
+		});
+		
+		
+		
+		
+		
+		((ConfigurableApplicationContext) service).close();
 	}
 
 }
