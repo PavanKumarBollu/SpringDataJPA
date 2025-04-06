b27630097e18909fa4229604d3ef449458abc58d
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 02:53:58 +0530

    Sprind Data JPa Project to work with PatientTable of HMS

--- 01_SpringDataJPA/src/main/java/com/pavan/Application.java
+++ 01_SpringDataJPA/src/main/java/com/pavan/Application.java
@@ -5,11 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.pavan.bo.CoronaVaccine;
+import com.pavan.service.CoronoVaccineMgmtServiceImpl;
+import com.pavan.service.ICoronaVaccineMgmtService;
+
 @SpringBootApplication
 public class Application {
 
  	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Application.class, args);
+		ICoronaVaccineMgmtService serviceImpl = context.getBean(CoronoVaccineMgmtServiceImpl.class);
+		
+		
+		CoronaVaccine vaccine = new CoronaVaccine(null, "covidshield", "serum", "IND", 750.0, 2);
+		serviceImpl.registerVacine(vaccine);
+		
 		
 		
 		
--- 01_SpringDataJPA/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
+++ 01_SpringDataJPA/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
@@ -11,20 +11,18 @@ public class CoronoVaccineMgmtServiceImpl implements ICoronaVaccineMgmtService {
 
 	@Autowired
 	private ICoronaVaccineRepo repo;
-	
-	
+
 	@Override
 	public String registerVacine(CoronaVaccine vaccine) {
 		System.out.println("In Memory Proxy Class is :: " + repo.getClass().getName());
 		CoronaVaccine saveVaccine = null;
-		
-		if(vaccine != null)
-		{
-			saveVaccine =	repo.save(vaccine);
+
+		if (vaccine != null) {
+			saveVaccine = repo.save(vaccine);
 		}
 		return saveVaccine != null ? "vaccine registered succesfully with " + saveVaccine.getRegNo()
-		: "vaccine registration failed";
-	
+				: "vaccine registration failed";
+
 	}
 
 }
