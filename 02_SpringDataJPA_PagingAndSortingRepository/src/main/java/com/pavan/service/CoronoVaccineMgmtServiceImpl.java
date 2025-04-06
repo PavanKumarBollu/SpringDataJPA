04292cbe8c901b73966af46a956dbd07c381e7cb
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 05:07:48 +0530

    SpringDataJpa Project To Work With PaggingAndSortingRepository

--- 02_SpringDataJPA_PagingAndSortingRepository/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
+++ 02_SpringDataJPA_PagingAndSortingRepository/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
@@ -1,33 +1,34 @@
 package com.pavan.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pavan.bo.CoronaVaccine;
 import com.pavan.dao.ICoronaVaccineRepo;
 
-@Service(value="service")
+@Service(value = "service")
 public class CoronoVaccineMgmtServiceImpl implements ICoronaVaccineMgmtService {
 
 	@Autowired
 	private ICoronaVaccineRepo repo;
-	
-	
+
 	@Override
 	public Iterable<CoronaVaccine> fetchDetails(boolean asc, String... properties) {
-		return null;
+		System.out.println("Implementation Class name of the proxy class :: " + repo.getClass().getName());
+		Sort sort = Sort.by(asc ? Direction.ASC : Direction.DESC);
+		return repo.findAll(sort);
 	}
-
 
 	@Override
 	public Iterable<CoronaVaccine> fetchDetailsByPageNo(int pageNo, int pageSize, boolean asc, String... properties) {
 		return null;
 	}
 
-
 	@Override
 	public void fetchDetailsByPagination(int pageSize) {
-		
+
 	}
 
 }
