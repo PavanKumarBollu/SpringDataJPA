74304d6414544d747d71bf6e89510ae2c55077df
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 04:47:52 +0530

    SpringDataJpa Project To Work With PaggingAndSortingRepository

---
+++ 02_SpringDataJPA_PagingAndSortingRepository/src/main/java/com/pavan/dao/ICoronaVaccineRepo.java
@@ -0,0 +1,7 @@
+package com.pavan.dao;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ICoronaVaccineRepo extends PagingAndSortingRepository<T, ID> {
+
+}
