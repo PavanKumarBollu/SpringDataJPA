0b75a602fbc4a5c99afd4167ac649844040ebaa6
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 02:50:23 +0530

    Sprind Data JPa Project to work with PatientTable of HMS

--- 01_SpringDataJPA/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
+++ 01_SpringDataJPA/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
@@ -1,13 +1,29 @@
 package com.pavan.service;
 
-import com.pavan.bo.CoronaVaccine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.pavan.bo.CoronaVaccine;
+import com.pavan.dao.ICoronaVaccineRepo;
+
+@Service(value = "service")
 public class CoronoVaccineMgmtServiceImpl implements ICoronaVaccineMgmtService {
 
+	@Autowired
+	private ICoronaVaccineRepo repo;
+	
+	
 	@Override
 	public String registerVacine(CoronaVaccine vaccine) {
-
-	
+		System.out.println("In Memory Proxy Class is :: " + repo.getClass().getName());
+		CoronaVaccine saveVaccine = null;
+		
+		if(vaccine != null)
+		{
+			saveVaccine =	repo.save(vaccine);
+		}
+		return saveVaccine != null ? "vaccine registered succesfully with " + saveVaccine.getRegNo()
+		: "vaccine registration failed";
 	
 	}
 
