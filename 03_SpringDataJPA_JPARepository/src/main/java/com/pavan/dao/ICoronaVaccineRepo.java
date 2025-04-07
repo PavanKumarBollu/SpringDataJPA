311946c4b062c6e699219304b4130cce881aee52
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 15:14:51 +0530

    SpringDataJpa Project To Work With JPA Repository

--- 03_SpringDataJPA_JPARepository/src/main/java/com/pavan/dao/ICoronaVaccineRepo.java
+++ 03_SpringDataJPA_JPARepository/src/main/java/com/pavan/dao/ICoronaVaccineRepo.java
@@ -1,11 +1,12 @@
 package com.pavan.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 
 import com.pavan.bo.CoronaVaccine;
 
-public interface ICoronaVaccineRepo extends PagingAndSortingRepository<CoronaVaccine, Long>, CrudRepository<CoronaVaccine, Long> {
+public interface ICoronaVaccineRepo extends JpaRepository<CoronaVaccine, Long> {
 
 }
