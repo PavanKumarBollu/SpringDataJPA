6fc2e7557989fc104bfa05fc2edcb5db0be1ee95
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 15:18:06 +0530

    SpringDataJpa Project To Work With JPA Repository

--- 03_SpringDataJPA_JPARepository/src/main/java/com/pavan/service/ICoronaVaccineMgmtService.java
+++ 03_SpringDataJPA_JPARepository/src/main/java/com/pavan/service/ICoronaVaccineMgmtService.java
@@ -1,5 +1,9 @@
 package com.pavan.service;
 
+import java.util.List;
+
+import com.pavan.bo.CoronaVaccine;
+
 public interface ICoronaVaccineMgmtService {
-	
+	public List<CoronaVaccine> searchVaccinesByGivenData(CoronaVaccine vaccine, boolean ascOrder,String... properties);
 }
