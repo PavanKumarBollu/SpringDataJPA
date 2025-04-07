791e7b6727d9f4c7b9023a69dd028105e8dd98a2
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 19:12:37 +0530

    SpringDataJpa Project To Work With Custom Queries

--- 05_SpringDataJpa_CustomQueryApp/src/main/java/com/pavan/dao/ICoronaVaccineRepo.java
+++ 05_SpringDataJpa_CustomQueryApp/src/main/java/com/pavan/dao/ICoronaVaccineRepo.java
@@ -1,9 +1,23 @@
 package com.pavan.dao;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pavan.bo.CoronaVaccine;
 
 public interface ICoronaVaccineRepo extends JpaRepository<CoronaVaccine, Long> {
+	public List<CoronaVaccine> findByCompany(String company);
 
+	public List<CoronaVaccine> findByCompanyEquals(String company);
+
+	public List<CoronaVaccine> findByCompanyIs(String company);
+
+	public List<CoronaVaccine> findByPriceLessThan(Double price);
+
+	public List<CoronaVaccine> findByCountryInAndPriceBetween(Collection<String> countries, double startRange,
+			double endRange);
+
+	public List<CoronaVaccine> findByCountryNotIn(Collection<String> countries);
 }
