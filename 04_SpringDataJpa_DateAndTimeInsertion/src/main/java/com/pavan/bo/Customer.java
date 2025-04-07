55cc9935c9b84aede16602755895adc0716d60ff
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 18:18:55 +0530

    SpringDataJpa Project To Work With Date and time

---
+++ 04_SpringDataJpa_DateAndTimeInsertion/src/main/java/com/pavan/bo/Customer.java
@@ -0,0 +1,17 @@
+package com.pavan.bo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Customer {
+
+	
+	private Integer cid;
+	private String cname;
+	private String caddress;
+
+	private LocalDateTime dob;
+	private LocalTime dom;
+	private LocalDate doj;
+}
