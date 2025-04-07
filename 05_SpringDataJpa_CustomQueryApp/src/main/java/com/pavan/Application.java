7feeac91289bab86afb2c0ed8fd3c4f110f909d8
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 19:17:09 +0530

    SpringDataJpa Project To Work With Custom Queries

--- 05_SpringDataJpa_CustomQueryApp/src/main/java/com/pavan/Application.java
+++ 05_SpringDataJpa_CustomQueryApp/src/main/java/com/pavan/Application.java
@@ -1,14 +1,33 @@
 package com.pavan;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.pavan.service.ICoronaVaccineMgmtService;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		
+		ApplicationContext factory = SpringApplication.run(Application.class, args);
+		ICoronaVaccineMgmtService service = factory.getBean(ICoronaVaccineMgmtService.class);
+
+//		 service.fetchVaccineByCompany("serum").forEach(System.out::println);
+//		 service.fetchVaccineByPricesLessThan(450.5).forEach(System.out::println);
+
+		List<String> countries = new ArrayList<String>();
+		countries.add("CHINA");
+		countries.add("Russia");
+		// service.searchVaccineByCountriesAndPriceRange(countries, 200.0,
+		// 500.0).forEach(System.out::println);
+
+		service.fetchVaccineNotByCountry(countries).forEach(System.out::println);;
+		((ConfigurableApplicationContext) factory).close();
 		
 	}
 
