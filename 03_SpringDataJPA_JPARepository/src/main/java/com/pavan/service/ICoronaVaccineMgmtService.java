bfe7a421e067cdf93a99681d0f91d6473343a2d4
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 17:52:26 +0530

    SpringDataJpa Project To Work With JPA Repository

--- 03_SpringDataJPA_JPARepository/src/main/java/com/pavan/service/ICoronaVaccineMgmtService.java
+++ 03_SpringDataJPA_JPARepository/src/main/java/com/pavan/service/ICoronaVaccineMgmtService.java
@@ -6,4 +6,6 @@ import com.pavan.bo.CoronaVaccine;
 
 public interface ICoronaVaccineMgmtService {
 	public List<CoronaVaccine> searchVaccinesByGivenData(CoronaVaccine vaccine, boolean ascOrder,String... properties);
+	public CoronaVaccine getVaccineById(Long regNo);
+	public String removeVaccinesByRegNo(Iterable<Long> regNo);
 }
