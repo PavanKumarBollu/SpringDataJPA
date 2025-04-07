317c2f734b48789f7ea5a60ad0a2c9fbd383b4a5
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 19:11:22 +0530

    SpringDataJpa Project To Work With Custom Queries

--- 05_SpringDataJpa_CustomQueryApp/src/main/java/com/pavan/bo/CoronaVaccine.java
+++ 05_SpringDataJpa_CustomQueryApp/src/main/java/com/pavan/bo/CoronaVaccine.java
@@ -2,12 +2,41 @@ package com.pavan.bo;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class CoronaVaccine implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long regNo;
+
+	@NonNull
 	private String name;
+	
+	@NonNull
 	private String company;
+	
+	@NonNull
 	private String country;
+	
+	@NonNull
 	private Double price;
+	
+	@NonNull
 	private Integer requriedDoseCount;
 }
--- 05_SpringDataJpa_CustomQueryApp/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
+++ 05_SpringDataJpa_CustomQueryApp/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
@@ -1,6 +1,15 @@
 
 package com.pavan.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pavan.dao.ICoronaVaccineRepo;
+
+@Service(value="service")
 public class CoronoVaccineMgmtServiceImpl implements ICoronaVaccineMgmtService {
+	
+	@Autowired
+	private ICoronaVaccineRepo repo;
 
 }
