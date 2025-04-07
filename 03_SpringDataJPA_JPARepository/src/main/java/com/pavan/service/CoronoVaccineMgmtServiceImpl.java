1e421021cf836645048ebba4bba8f6dbfe3cbe25
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 15:18:24 +0530

    SpringDataJpa Project To Work With JPA Repository

--- 03_SpringDataJPA_JPARepository/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
+++ 03_SpringDataJPA_JPARepository/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
@@ -17,44 +17,7 @@ public class CoronoVaccineMgmtServiceImpl implements ICoronaVaccineMgmtService {
 	@Autowired
 	private ICoronaVaccineRepo repo;
 
-	@Override
-	public Iterable<CoronaVaccine> fetchDetails(boolean asc, String... properties) {
-		System.out.println("Implementation Class name of the proxy class :: " + repo.getClass().getName());
-		Sort sort = Sort.by(asc ? Direction.ASC : Direction.DESC,properties);
-		return repo.findAll(sort);
-	}
-
-	@Override
-	public Iterable<CoronaVaccine> fetchDetailsByPageNo(int pageNo, int pageSize, boolean asc, String... properties) {
-		Pageable pageRequest = PageRequest.of(pageNo, pageSize, asc?Direction.ASC : Direction.DESC, properties);
-		Page<CoronaVaccine> page = repo.findAll(pageRequest);
-		return page;
-	}
-
-	@Override
-	public void fetchDetailsByPagination(int pageSize) {
-		
-		// total table records count
-		
-		long totalRows = repo.count();
-		
-		// Setting up the total page count based on the number of records
-		
-		Long totalPageCount = totalRows / pageSize;
-		
-		totalPageCount = totalRows % pageSize == 0 ? totalPageCount : ++totalPageCount;
-		
-		
-		for(int i =0 ; i< totalPageCount; i++)
-		{
-			Pageable page = PageRequest.of(i, pageSize);
-			
-			Page<CoronaVaccine> pag = repo.findAll(page);
-			
-			pag.getContent().forEach(System.out::println);
-			System.out.println("-------- " + (i+1) + " page Of " + pag.getTotalPages());
-		}
-
+	
 	}
 
 }
