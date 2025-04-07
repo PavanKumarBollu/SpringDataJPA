266a02338623a0eb7465b9e6889c40d8de18837e
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 19:12:09 +0530

    SpringDataJpa Project To Work With Custom Queries

--- 05_SpringDataJpa_CustomQueryApp/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
+++ 05_SpringDataJpa_CustomQueryApp/src/main/java/com/pavan/service/CoronoVaccineMgmtServiceImpl.java
@@ -1,9 +1,13 @@
 
 package com.pavan.service;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pavan.bo.CoronaVaccine;
 import com.pavan.dao.ICoronaVaccineRepo;
 
 @Service(value="service")
@@ -11,5 +15,42 @@ public class CoronoVaccineMgmtServiceImpl implements ICoronaVaccineMgmtService {
 	
 	@Autowired
 	private ICoronaVaccineRepo repo;
+
+	@Override
+	public List<CoronaVaccine> findByCompany(String company) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CoronaVaccine> findByCompanyEquals(String company) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CoronaVaccine> findByCompanyIs(String company) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CoronaVaccine> findByPriceLessThan(Double price) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CoronaVaccine> findByCountryInAndPriceBetween(Collection<String> countries, double startRange,
+			double endRange) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CoronaVaccine> findByCountryNotIn(Collection<String> countries) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
