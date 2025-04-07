bfd105eca6a49e8f53b35979e25f6953f9d0a44c
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 18:20:55 +0530

    SpringDataJpa Project To Work With Date and time

---
+++ 04_SpringDataJpa_DateAndTimeInsertion/src/main/java/com/pavan/service/CustomerMgmtServieImpl.java
@@ -0,0 +1,21 @@
+package com.pavan.service;
+
+import java.util.List;
+
+import com.pavan.bo.Customer;
+
+public class CustomerMgmtServieImpl implements ICustomerMgmtService {
+
+	@Override
+	public String registerCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> getAllCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
