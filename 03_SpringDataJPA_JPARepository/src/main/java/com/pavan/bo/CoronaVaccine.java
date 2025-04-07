4be81753397c616d2369cc99b8e68fc0a7b93262
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 15:17:42 +0530

    SpringDataJpa Project To Work With JPA Repository

--- 03_SpringDataJPA_JPARepository/src/main/java/com/pavan/bo/CoronaVaccine.java
+++ 03_SpringDataJPA_JPARepository/src/main/java/com/pavan/bo/CoronaVaccine.java
@@ -9,25 +9,31 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class CoronaVaccine implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long regNo;
 	
+	@NonNull
 	private String name;
+	@NonNull
 	private String company;
+	@NonNull
 	private String country;
+	@NonNull
 	private Double price;
+	@NonNull
 	private Integer requriedDoseCount;
 
 }
