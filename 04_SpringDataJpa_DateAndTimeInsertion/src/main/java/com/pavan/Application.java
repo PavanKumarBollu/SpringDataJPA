88761dbbe591eb3b3d9ebf0c8c4555e97fb2eff0
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 18:40:24 +0530

    SpringDataJpa Project To Work With Date and time

--- 04_SpringDataJpa_DateAndTimeInsertion/src/main/java/com/pavan/Application.java
+++ 04_SpringDataJpa_DateAndTimeInsertion/src/main/java/com/pavan/Application.java
@@ -1,13 +1,31 @@
 package com.pavan;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.pavan.bo.Customer;
+import com.pavan.service.CustomerMgmtServieImpl;
+import com.pavan.service.ICustomerMgmtService;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		
+		
+		 ICustomerMgmtService servieImpl = context.getBean(CustomerMgmtServieImpl.class);
+		
+		 Customer customer = new Customer(1, "pavan", "hyderabad", LocalDateTime.of(2001, 8, 23, 6, 15), LocalTime.of(10, 15), LocalDate.now());
+		 
+		 servieImpl.registerCustomer(customer);
+		
+		((ConfigurableApplicationContext) context).close();
 	}
 
 }
